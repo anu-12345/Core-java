cdca574451ac0c8ce3edea4fe52886ae2f624b14
anu-12345 <55178866+anu-12345@users.noreply.github.com>
2021-01-04 09:31:27 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,14 @@
+class Fibonacci{
+public static void main(String args[]){
+int sum1 = 0; sum2 = 1; sum3 = 0;
+
+for(int i=0; i<=10; i++){
+
+sum3 = sum1+sum2;
+sum1=sum2;
+sum2=sum3;
+}
+System.out.println(sum3):
+
+}
+}
